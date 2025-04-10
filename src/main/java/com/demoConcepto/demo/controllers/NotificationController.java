4a2d93c156220547efc1510c07fb322556b5836b
brianmachado18 <andrs.machado1@gmail.com>
2025-04-10 19:19:49 -0300

    arreglo pass

--- src/main/java/com/demoConcepto/demo/controllers/NotificationController.java
+++ src/main/java/com/demoConcepto/demo/controllers/NotificationController.java
@@ -3,13 +3,12 @@ package com.demoConcepto.demo.controllers;
 import com.demoConcepto.demo.dto.MailRequest;
 import com.demoConcepto.demo.services.MailService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class NotificacionController {
+public class NotificationController {
     @Autowired
     private MailService mailService;
 
